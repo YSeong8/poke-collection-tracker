b93b773c101ad71ee250789f0b1396df0a2fa0c1
YSeong8 <156128116+YSeong8@users.noreply.github.com>
2026-04-13 23:34:20 -0400

    Add static file serving and catch-all route
    Serve static files from the client distribution folder and handle all routes by sending the index.html file.

--- server/index.js
+++ server/index.js
@@ -521,6 +521,12 @@ app.get("/api/compare/:shareCode", requireAuth, (req, res) => {
   );
 });
 
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
