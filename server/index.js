30d2db4815ff00066adc5711323c51bf7577a4d4
YSeong8 <156128116+YSeong8@users.noreply.github.com>
2026-04-13 23:22:37 -0400

    Set server to listen on specified PORT

--- server/index.js
+++ server/index.js
@@ -520,6 +520,7 @@ app.get("/api/compare/:shareCode", requireAuth, (req, res) => {
   );
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
